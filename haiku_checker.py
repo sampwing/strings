ecb60f9a38aae9ed9410fadb7ff0c6b2b5cc83a8
Sam Wing <sampwing@gmail.com>
2013-05-02 23:49:58 -0700

    comments

--- haiku_checker.py
+++ haiku_checker.py
@@ -4,6 +4,7 @@ import re
 HAIKU = [5,7,5]
 
 def is_haiku(haiku):
+    """take a haiku with each line newline delineated and determine whether it is a haiku or not"""
     strings = re.split(r'[\n\r]+', haiku)
     return [TextStatistics(string).count_syllables() for string in strings] == HAIKU
 
